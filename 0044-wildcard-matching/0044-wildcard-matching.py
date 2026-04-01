ec534e428c8277225a01055fee130c553e6c5ffe
soujanyabhirade <149173159+soujanyabhirade@users.noreply.github.com>
2026-04-01 19:20:55 +0530

    Time: 1013 ms (39.27%), Space: 43.8 MB (20.08%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -11,7 +11,7 @@ class Solution:
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if p[j - 1] == '*':
-                    dp[i][j] = dp[i][j - 1] or dp[i - 1][j]
+                    dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
                 elif p[j - 1] == '?' or p[j - 1] == s[i - 1]:
                     dp[i][j] = dp[i - 1][j - 1]
 
