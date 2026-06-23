d7c2afb541ecc299eda712145f8cc787ba295a09
soujanyabhirade <149173159+soujanyabhirade@users.noreply.github.com>
2026-06-23 21:06:51 +0530

    Time: 7 ms (57.18%), Space: 12.5 MB (23.25%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,19 +6,17 @@ class Solution:
 
         for r in range(9):
             for c in range(9):
-
                 val = board[r][c]
-
-                if val == ".":
+                if val == '.':
                     continue
 
-                box_index = (r // 3) * 3 + (c // 3)
+                box = (r // 3) * 3 + (c // 3)
 
-                if val in rows[r] or val in cols[c] or val in boxes[box_index]:
+                if val in rows[r] or val in cols[c] or val in boxes[box]:
                     return False
 
                 rows[r].add(val)
                 cols[c].add(val)
-                boxes[box_index].add(val)
+                boxes[box].add(val)
 
         return True
